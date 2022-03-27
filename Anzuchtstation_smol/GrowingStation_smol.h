368cd0b6e0fa1575a6f500dea8c019a50f8cdc08
Mikagino <angeli.mika@gmail.com>
2022-03-27 16:58:41 +0200

    Changed watering_treshold

--- Anzuchtstation_smol/GrowingStation_smol.h
+++ Anzuchtstation_smol/GrowingStation_smol.h
@@ -77,7 +77,7 @@ class GrowingStation_smol{
   unsigned long loop_delay = 5000; // - delay between entire loops -
   unsigned int watering_frequencyH = 24; // - how often it's watered (in hours) -
   unsigned long watering_amount = 5000; // - how long the pump is activated for watering
-  unsigned long watering_treshold = 3300; // - on what soilHumidity the pump will be activated -
+  unsigned long watering_treshold = 2000; // - on what soilHumidity the pump will be activated -
 
 
   // +--+ telegram bot +--+

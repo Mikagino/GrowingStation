78355653c42823d0d77c16eb33f2d3426a389002
Mikagino <angeli.mika@gmail.com>
2025-04-19 10:29:48 +0200

    dunno man

--- GrowingStationStartup_unterstation.bat
+++ GrowingStationStartup_unterstation.bat
@@ -1,3 +1,3 @@
 @echo off
 D:\Programmieren\GitHub\GrowingStation\Anzuchtstation_smol\Anzuchtstation_smol.ino
-start devenv -command File.OpenProject "D:\Programmieren\GitHub\GrowingStation\Anzuchtstation_smol"
+start devenv "D:\Programmieren\GitHub\GrowingStation\Anzuchtstation_smol"
